e20694f1dca7e18c2c1bce843909da013c449080
showdownsj <dezel321@gmail.com>
2018-06-26 22:59:11 +0300

    a little fixes

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
-import HomeScreen from './Screens/HomeScren';
+import HomeScreen from './Screens/HomeScreen';
 import ProfileScreen from './Screens/SecondScreen';
 
 
