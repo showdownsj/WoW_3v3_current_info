1a52f49f6a8012cf1873a22a6e2e097740cbb02b
Aliaksei Valuyevich <aliaksei.valuyevich@epolsoft.com>
2018-06-28 16:04:06 +0300

    added new functional (pvpmode). some changes in the styles.

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Picker, YellowBox } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Picker, YellowBox, Keyboard } from 'react-native';
+
 import axios from 'axios';
 import * as Realms from './../resources/en_GBRealms.json';
 
-YellowBox.ignoreWarnings(['Warning: Each child in an array or iterator should have a unique "key" prop']);
+YellowBox.ignoreWarnings(['Warning: Each child in an array or iterator should have a unique "key" prop', 'Warning: Failed prop type: Invalid props.style key `color` supplied to `View`']);
 const generateRand = () => {
   return (Math.random() * 256) ^ 0;
 };
@@ -16,8 +17,8 @@ const getFromBlizz = async (realm, name) => {
   return result;
 };
 
-const getStatBlizz = async (realm, name) =>{
-  const path = `https://eu.api.battle.net/wow/character/${realm}/${name}?fields=statistics&locale=en_US&apikey=6x24nk9hj6d4b7efp8a35rsxavtq7twr`;
+const getStatBlizz = async (realm, name) => {
+  const path = `https://eu.api.battle.net/wow/character/${realm}/${name}?fields=statistics&locale=en_GB&apikey=6x24nk9hj6d4b7efp8a35rsxavtq7twr`;
   const result = await axios.get(path);
   // console.log(result);
   return result;
@@ -26,7 +27,9 @@ const getStatBlizz = async (realm, name) =>{
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Welcome',
-    
+    headerStyle: { backgroundColor: '#47271a', borderBottomColor: '#f8b700', borderBottomWidth: 2, },
+    headerTitleStyle: { color: '#f8b700' },
+
   };
 
   constructor(props) {
@@ -37,53 +40,71 @@ export default class HomeScreen extends React.Component {
       realmNames.push(realm.name);
     });
     realmNames.sort();
-    realmNames.forEach( realm => {
-      pickerData.push(<Picker.Item label={realm} value={realm} />);
+    realmNames.forEach(realm => {
+      pickerData.push(<Picker.Item key={pickerData.length} label={realm} value={realm} />);
     });
 
     this.state = {
       name: '',
       realm: realmNames[0],
       pickerData: pickerData,
-      error: false
+      error: false,
+      pvpMode: '3v3'
     };
 
- 
-     this.onHandlePress = this.onHandlePress.bind(this);
+
+    this.onHandlePress = this.onHandlePress.bind(this);
   }
 
 
   async onHandlePress() {
-    try{
-    const data = await getFromBlizz(this.state.realm, this.state.name);
-    const stats = await getStatBlizz(this.state.realm, this.state.name);
-   // console.log(JSON.stringify(stats));
-    //console.log(stats.data.statistics.subCategories[9].subCategories[0].statistics[24].quantity);
-   
-    this.setState({ color: `rgb(${generateRand()},${generateRand()},${generateRand()})`, error:false });
-    this.props.navigation.navigate('Profile', { data: data, statistics: stats});
+    try {
+      const data = await getFromBlizz(this.state.realm, this.state.name);
+      const stats = await getStatBlizz(this.state.realm, this.state.name);
+    // console.log(JSON.stringify(stats));
+      //console.log(stats.data.statistics.subCategories[9].subCategories[0].statistics[24].quantity);
+      this.props.navigation.navigate('Profile', { data: data, statistics: stats, pvpMode: this.state.pvpMode });
     }
-    catch(err){
+    catch (err) {
       console.log(err);
-      this.setState({error:true});
+      this.setState({ error: true });
     }
   }
+
+  containerTouched(event) {
+    this.refs.textInput.blur();
+    return false;
+  }
+
   render() {
-  
+
     //console.log(this.state);
     return (
-      <View style={styles.container}>
-       {this.state.error && <View style={styles.wrong}><Text style={{color:'white'}}>{'Unkown player. Please check the input name/realm'}</Text></View>}
-        <TextInput onChangeText={(text) => { this.setState({ name: text || this.state.name }) }} style={styles.button} />
-        <Picker selectedValue={this.state.realm} onValueChange={(itemValue, itemIndex) => this.setState({ realm: itemValue })} style={styles.button} >
-         
-         {this.state.pickerData}
-         
+
+      <View style={styles.container} onStartShouldSetResponder={this.containerTouched.bind(this)}>
+        {this.state.error && <View style={styles.wrong}><Text style={{ color: 'white', textAlign: 'center', fontSize: 14, fontFamily: 'Roboto' }}>{'Unkown player. Please check the input name/realm'}</Text></View>}
+        <TextInput ref='textInput'
+          onChangeText={(text) => { this.setState({ name: text || this.state.name }) }}
+          style={styles.textInput}
+          onSubmit={Keyboard.dismiss}
+          placeholder='Character name'
+          placeholderTextColor={'#f8b700'}
+          clearTextOnFocus={true}
+          underlineColorAndroid='transparent'
+        />
+        <Picker selectedValue={this.state.realm} onValueChange={(itemValue, itemIndex) => { this.setState({ realm: itemValue }); Keyboard.dismiss} } style={styles.picker} >
+            {this.state.pickerData}
+        </Picker>
+        <Picker selectedValue={this.state.pvpMode}
+          onValueChange={(itemValue, itemIndex) =>{ this.setState({ pvpMode: itemValue }); Keyboard.dismiss} }
+          style={styles.picker}>
+          <Picker.Item label='2v2' value='2v2' />
+          <Picker.Item label='3v3' value='3v3' />
+          <Picker.Item label='RBG' value='RBG' />
         </Picker>
         <TouchableOpacity onPress={this.onHandlePress} style={styles.button} >
-          <Text style={{color: '#f8b700'}}> Press </Text>
+          <Text style={{ color: '#f8b700' }}> Press </Text>
         </TouchableOpacity>
-
       </View>
     );
   }
@@ -96,26 +117,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    marginTop: 15,
+    borderRadius: 5,
+    backgroundColor: '#89320f',
+    color: '#f8b700',
+    padding: 10,
+    width: 250
+  },
+  picker: {
+    alignSelf: 'center',
+    marginTop: 15,
+    borderRadius: 5,
+    backgroundColor: '#89320f',
+    color: '#f8b700',
+    padding: 10,
+    width: 250,
+  },
+
   button: {
     marginTop: 15,
-   // border: none;
     borderRadius: 5,
-   // color: '#fafafa',
     alignItems: 'center',
-    backgroundColor: '#ff6130',
+    backgroundColor: '#89320f',
+    alignItems: 'center',
+    color: '#f8b700',
     padding: 10,
-    width: 200
+    width: 250
   },
   wrong: {
     alignItems: 'center',
-    backgroundColor: 'red',
-   
+    backgroundColor: '#c12501',
     padding: 10,
-    width: 200
-  },
-  contentContainer: {
-    paddingVertical: 20,
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD'
+    width: 250
   }
 });
--- Screens/SecondScreen.js
+++ Screens/SecondScreen.js
@@ -1,35 +1,82 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button, FlatList, Image} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, Image } from 'react-native';
 
 
 export default class ProfileScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Player\'s details for 3v3',
+  static navigationOptions = ({ navigation }) => {
+    return ({
+      title: `Player\'s details for ${navigation.getParam('pvpMode')}`,
+      headerStyle: { backgroundColor: '#47271a', borderBottomColor: '#f8b700', borderBottomWidth: 2, },
+      headerTitleStyle: { color: '#f8b700' }
+    }
+    );
   };
   constructor(props) {
     super(props);
     const data = this.props.navigation.state.params.data.data;
     const statistics = this.props.navigation.state.params.statistics;
-    this.state =  {data:data, 
-                  icon: `https://render-eu.worldofwarcraft.com/character/${data.thumbnail}`,
-                  statistics: statistics };
+    const pvpMode = this.props.navigation.state.params.pvpMode;
+    this.state = {
+      data: this._parseDataViaPVPMode(pvpMode, data, statistics),
+      icon: `https://render-eu.worldofwarcraft.com/character/${data.thumbnail}`
+    };
+  }
+
+
+  _parseDataViaPVPMode(pvpMode, data, statistics) {
+
+    let arenaData;
+
+    switch (pvpMode) {
+      case '3v3':
+        arenaData = {
+          name: `${data.name}-${data.realm}`,
+          rating: data.pvp.brackets.ARENA_BRACKET_3v3.rating,
+          seasonPlayed: data.pvp.brackets.ARENA_BRACKET_3v3.seasonPlayed,
+          seasonWon: data.pvp.brackets.ARENA_BRACKET_3v3.seasonWon,
+          highestRating: statistics.data.statistics.subCategories[9].subCategories[0].statistics[24].quantity
+        };
+        return arenaData; break;
+
+      case '2v2':
+        arenaData = {
+          name: `${data.name}-${data.realm}`,
+          rating: data.pvp.brackets.ARENA_BRACKET_2v2.rating,
+          seasonPlayed: data.pvp.brackets.ARENA_BRACKET_2v2.seasonPlayed,
+          seasonWon: data.pvp.brackets.ARENA_BRACKET_2v2.seasonWon,
+          highestRating: statistics.data.statistics.subCategories[9].subCategories[0].statistics[25].quantity
+        };
+        return arenaData; break;
+
+      case 'RBG':
+        arenaData = {
+          name: `${data.name}-${data.realm}`,
+          rating: data.pvp.brackets.ARENA_BRACKET_RBG.rating,
+          seasonPlayed: data.pvp.brackets.ARENA_BRACKET_RBG.seasonPlayed,
+          seasonWon: data.pvp.brackets.ARENA_BRACKET_RBG.seasonWon
+        };
+        return arenaData; break;
+
+      default: return null;
+    }
+
   }
 
   render() {
     //console.log(JSON.stringify(this.props.navigation.state.params));
     //console.log(this.state.icon);
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Image source={{uri: this.state.icon}} style={{width: 84, height: 84, marginTop: 5}} />
+      <View style={styles.container}>
+        <Image source={{ uri: this.state.icon }} style={{ width: 84, height: 84, marginTop: 10 }} />
         <FlatList
           data={[
-            {key: `${this.state.data.name}-${this.state.data.realm}` },
-            {key: `Rating: ${this.state.data.pvp.brackets.ARENA_BRACKET_3v3.rating}`},
-            {key: `Games played: ${this.state.data.pvp.brackets.ARENA_BRACKET_3v3.seasonPlayed}`},
-            {key: `Wins: ${this.state.data.pvp.brackets.ARENA_BRACKET_3v3.seasonWon}`},
-            {key: `Highest 3 man personal rating: ${this.state.statistics.data.statistics.subCategories[9].subCategories[0].statistics[24].quantity}`}
+            { key: `${this.state.data.name}` },
+            { key: `Rating: ${this.state.data.rating}` },
+            { key: `Games played: ${this.state.data.seasonPlayed}` },
+            { key: `Wins: ${this.state.data.seasonWon}` },
+            { key: this.state.data.highestRating ? `Highest personal rating: ${this.state.data.highestRating}`:''}
           ]}
-          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+          renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
         />
       </View>);
   }
@@ -37,12 +84,15 @@ export default class ProfileScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 22
+    flex: 1,
+    backgroundColor: '#211510',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   item: {
     padding: 10,
     fontSize: 16,
     height: 44,
+    color: '#f8b700',
   },
 })
