b489d1ef05c711fb7a042d60144c846bd408fac5
showdownsj <dezel321@gmail.com>
2018-06-28 20:39:19 +0300

    path to others features

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -4,6 +4,13 @@ import { StyleSheet, Text, View, TextInput, TouchableOpacity, Picker, YellowBox,
 import axios from 'axios';
 import * as Realms from './../resources/en_GBRealms.json';
 
+
+/* get requests:
+https://eu.api.battle.net/wow/data/character/classes?locale=en_GB&apikey=6x24nk9hj6d4b7efp8a35rsxavtq7twr   classes
+https://eu.api.battle.net/wow/data/character/achievements?locale=en_GB&apikey=6x24nk9hj6d4b7efp8a35rsxavtq7twr allachieve
+https://us.api.battle.net/wow/leaderboard/3v3?locale=en_US&apikey=6x24nk9hj6d4b7efp8a35rsxavtq7twr    leaderdb.
+https://us.api.battle.net/wow/character/test-realm/Peratryn?fields=achievements&locale=en_US&apikey=6x24nk9hj6d4b7efp8a35rsxavtq7twr
+ */
 YellowBox.ignoreWarnings(['Warning: Each child in an array or iterator should have a unique "key" prop', 'Warning: Failed prop type: Invalid props.style key `color` supplied to `View`']);
 const generateRand = () => {
   return (Math.random() * 256) ^ 0;
